c579d65708a88bf06a6e8779ffe0b557f6615071
Bünyamin Macit <macitbunyamin@gmail.com>
2019-02-13 14:33:08 +0300

    mailgonderme

--- Wissen/App_Start/RouteConfig.cs
+++ Wissen/App_Start/RouteConfig.cs
@@ -12,8 +12,9 @@ namespace Wissen
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(name: "Contact", url: "iletisim",
+              defaults: new { controller = "Home", action = "Contact" });
             routes.MapRoute(name: "About", url: "hakkimizda",
-
                 defaults: new { controller = "Home", action = "About" });
             routes.MapRoute(
                 name: "Default",
--- Wissen/Controllers/HomeController.cs
+++ Wissen/Controllers/HomeController.cs
@@ -26,5 +26,35 @@ namespace Wissen.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult Contact(string firstName, string lastName, string email, string phone, string subject,string message)
+        {
+            //
+            //todoMail gönderme
+            System.Net.Mail.MailMessage mailMessage = new System.Net.Mail.MailMessage();
+
+            mailMessage.From = new System.Net.Mail.MailAddress("macitbunyamin@gmail.com", "Gönderen Firma Adı");
+            mailMessage.Subject = "İletişim Formu: " + firstName +lastName;
+
+            mailMessage.To.Add("macitbunyamin@gmail.com");
+
+            string body;
+            body = "Ad Soyad: " + firstName+ lastName +"<br />";
+            body += "Telefon: " +phone+ "<br />";
+            body += "E-posta: " + email+ "<br />";
+            body += "Konu: " + subject + "<br />";
+            body += "Mesaj: " +message + "<br />";
+            body += "Tarih: " + DateTime.Now.ToString("dd MMMM yyyy") + "<br />";
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = body;
+
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new System.Net.NetworkCredential("macitbunyamin@gmail.com", "gönderici sifresi");
+            smtp.EnableSsl = true;
+            smtp.Send(mailMessage);
+           
+            ViewBag.Message = "Form başarıyla ileildi,en kısa zamanda dönüş yapacağız...";
+            return View();
+        }
     }
 }
